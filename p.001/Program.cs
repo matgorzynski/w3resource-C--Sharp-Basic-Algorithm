abe761a917775d488a472036f751e38657fca9b0
Mateusz Górzyński <mateuszgorzynski@hotmail.com>
2020-11-07 09:58:02 +0100

    add first tasks

---
+++ p.001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace p._001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split();
+            
+            
+            while(line[0] != "")
+            {
+                Console.WriteLine(Int32.Parse(line[0]) + Int32.Parse(line[1]));
+                line = Console.ReadLine()?.Split();
+            }
+            return;
+        }
+    }
+}
