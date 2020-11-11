6c2333061b8a17e53849842bcfb75e27980b79a6
Mateusz Górzyński <mateuszgorzynski@hotmail.com>
2020-11-11 12:57:31 +0100

    add tasks 2 and 3

---
+++ p.002/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace p._002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+
+            while (line != "")
+            {
+                int number = Int32.Parse(line);
+                if (number >= 51)
+                {
+                    Console.WriteLine((number - 51) * 3);
+                }
+                else
+                {
+                    Console.WriteLine(51 - number);
+                }
+                line = Console.ReadLine();
+            }
+            return;
+        }
+    }
+}
---
+++ p.003/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace p._003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(", ");
+
+
+            while (line[0] != "")
+            {
+                if (Int32.Parse(line[0]) == 30 || Int32.Parse(line[1]) == 30 || Int32.Parse(line[0])+ Int32.Parse(line[1])== 30)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+
+                line = Console.ReadLine().Split(", ");
+            }
+        }
+    }
+}
