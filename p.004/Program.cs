eed681833c72a7fdbd7ae559bc2983e3e0ccba99
Mateusz Górzyński <mateuszgorzynski@hotmail.com>
2020-11-23 22:18:01 +0100

    add p.004 and p.005 quest

---
+++ p.004/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace p._004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+            while (line != "")
+            {
+                int number = Int32.Parse(line);
+                if (Math.Abs(number - 100) <= 10 || Math.Abs(number - 200) <= 10)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ p.005/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace p._005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+            while (line != "")
+            {
+                if (line.Length > 2 && line.Substring(0, 2).Equals("if"))
+                {
+                    Console.WriteLine(line);
+                }
+                else
+                {
+                    Console.WriteLine("if " + line);
+                }
+                line = Console.ReadLine();
+            }
+        }
+    }
+
+}
