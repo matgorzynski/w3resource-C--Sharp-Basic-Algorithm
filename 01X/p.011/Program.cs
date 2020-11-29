7ded4d998628e3e325332f71b835241bd493bae0
Mateusz Górzyński <mateuszgorzynski@hotmail.com>
2020-11-29 13:58:59 +0100

    Remove eariler project to folder add 011, 012 solution

---
+++ 01X/p.011/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ *  
+ * Write a C# Sharp program to create a new string taking the first 3 characters of a given string 
+ * and return the string with the 3 characters added at both the front and back. 
+ * If the given string length is less than 3, use whatever characters are there.
+ * 
+ * Sample Input:
+ * "Python"
+ * "JS"
+ * "Code"
+ * Expected Output:
+ * PytPythonPyt
+ * JSJSJS
+ * CodCodeCod
+ * 
+ * 
+ */
+using System;
+using System.Linq;
+
+namespace p._011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "")
+                    return;
+
+                if(line.Length<=3)
+                {
+                    Console.WriteLine(string.Concat(Enumerable.Repeat(line, 3)));
+                }
+                else
+                {
+                    Console.WriteLine(line.Substring(0, 3) + line + line.Substring(0, 3));
+                }
+            }
+                    
+        }
+    }
+}
---
+++ 01X/p.012/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * 
+ * Write a C# Sharp program to check if a given string starts with 'C#' or not.
+ * 
+ * Sample Input:
+ * "C# Sharp"
+ * "C#"
+ * "C++"
+ * Expected Output:
+ * True
+ * True
+ * False
+ * 
+ */
+using System;
+
+namespace p._012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string line = Console.ReadLine();
+                if (line == "")
+                    return;
+
+                Console.WriteLine(line.Substring(0, 2).Contains("C#"));
+            }
+        }
+    }
+}
